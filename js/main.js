3e30a1e971a1e738f38a4262f84d4b356ad78758
heraudk <72447916+heraudk@users.noreply.github.com>
2020-11-23 17:53:07 +0100

    exo jquery

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(function(){   
+
+    $('button').on('click',function(){
+        $('.zonetexte').css({border:'2px solid red'});
+
+    });
+
+
+    
+    $('button').on('click',function(){
+        $('p').css({border:'2px solid red'});
+
+    });
+
+
+});
+
+
+
